06f61719acf1f123944fbef109df49eb9634297c
Ian Zolitor <ianzolitor@gmail.com>
2017-06-13 13:49:08 -0400

    add selector for last two sources

--- src/Selector.js
+++ src/Selector.js
@@ -4,7 +4,10 @@ import guardian from "./gaurdian.svg";
 import intercept from "./intercept.jpg";
 import billpenn from "./billypenn.jpg";
 import jacobin from "./jacobin.png";
+import bbc from "./bbc.png"
+import jazeera from "./jazeera.png"
 import axios from "axios";
+
 
 class Selector extends Component {
     constructor(props) {
@@ -64,6 +67,24 @@ class Selector extends Component {
                         onClick={this.selectSource}
                     />
                     <div id="5" onClick={this.removeSource}>Remove Source</div>
+                </div>
+                <div>
+                    <img
+                        src={jazeera}
+                        alt="Al Jazeera America"
+                        id="6"
+                        onClick={this.selectSource}
+                    />
+                    <div id="6" onClick={this.removeSource}>Remove Source</div>
+                </div>
+                <div>
+                    <img
+                        src={bbc}
+                        alt="BBC News"
+                        id="7"
+                        onClick={this.selectSource}
+                    />
+                    <div id="7" onClick={this.removeSource}>Remove Source</div>
                 </div>
             </div>
         );
